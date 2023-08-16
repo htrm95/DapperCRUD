3282c731c79e47042cb7e4348d956bcdaaecc105
htrm95 <h.rostaminezhad1995@gmail.com>
2023-08-16 16:49:24 +0330

    add create in customer

--- DapperCRUD/Controllers/CustomerController.cs
+++ DapperCRUD/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using DapperCRUD.Repository;
+﻿using DapperCRUD.Models;
+using DapperCRUD.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DapperCRUD.Controllers
@@ -16,11 +17,64 @@ namespace DapperCRUD.Controllers
             return View(result);
         }
 
-        //[HttpDelete, ActionName("DeleteConfirmed")]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    await _iCustomerRepository.Delete(id);
-        //    return RedirectToAction(nameof(Index));
-        //}
+        public async Task<IActionResult> Details(int id)
+        {
+            var result = await _iCustomerRepository.GetByIdAsync(id);
+            return View(result);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+
+                await _iCustomerRepository.Create(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var _iCustomer = await _iCustomerRepository.GetByIdAsync(id);
+            return View(_iCustomer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    customer.Id = id;
+
+                    await _iCustomerRepository.Update(customer);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var _iCustomer = await _iCustomerRepository.GetByIdAsync(id);
+            return View(_iCustomer);
+        }
+
+        [HttpDelete, ActionName("DeleteConfirmed")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _iCustomerRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- DapperCRUD/Repository/CustomerRepository.cs
+++ DapperCRUD/Repository/CustomerRepository.cs
@@ -23,6 +23,56 @@ namespace DapperCRUD.Repository
             return result.ToList();
 
         }
+        public async Task<CustomerDto> GetByIdAsync(int id)
+        {
+            //var query = "SELECT * FROM " + typeof(BranchDto).Name + " WHERE Id = @Id";
+            var query = "select CU.* bankname from Customer as CU  WHERE CU.Id = @Id";
+            var parameters = new DynamicParameters();
+            parameters.Add("Id",id, DbType.Int64);
+            using (var connection = _context.CreateConnection())
+            {
+                var result = await connection.QuerySingleOrDefaultAsync<CustomerDto>(query, new { id });
+                return result;
+            }
+        }
+        public async Task Create(Customer _Customer)
+        {
+            var query = "INSERT INTO " + typeof(Customer).Name + " (Name, Family,Phone,Mobile,NationalCode) VALUES (@Name, @Family,@Phone,@Mobile,@NationalCode)";
+            var parameters = new DynamicParameters();
+            parameters.Add("Name", _Customer.Name, DbType.String);
+            parameters.Add("Family", _Customer.Family, DbType.String);
+            parameters.Add("Phone", _Customer.Phone, DbType.String);
+            parameters.Add("Mobile", _Customer.Mobile, DbType.String);
+            parameters.Add("NationalCode", _Customer.NationalCode, DbType.String);
+
+            using var connection = _context.CreateConnection();
+
+            await connection.ExecuteAsync(query, parameters);
+
+        }
+        public async Task Update(Customer _Customer)
+        {
+            var query = "UPDATE Branch SET Name = @Name, Tel =@Tel    WHERE Id = @Id";
+            var parameters = new DynamicParameters();
+            parameters.Add("Id", _Customer.Id, DbType.Int64);
+            parameters.Add("Name", _Customer.Name, DbType.String);
+            parameters.Add("Tel", _Customer.NationalCode, DbType.String);
+            parameters.Add("Tel", _Customer.Phone, DbType.String);
+            parameters.Add("Tel", _Customer.Mobile, DbType.String);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+        public async Task Delete(int id)
+        {
+            var query = "DELETE FROM " + typeof(Customer).Name + " WHERE Id = @Id";
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, new { id });
+            }
+        }
 
     }
 }
